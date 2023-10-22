fcf82ab9697f44cae356ac7b6d1ca7515dcf6e99
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-22 14:37:27 +0530

    modified app.js added swagger.yaml

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 // connectDB
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
@@ -33,9 +38,13 @@ app.use(cors());
 app.use(xss());
 
 // extra packages
+
 app.get('/', (req, res) => {
-  res.send('Server is running successfully');
+  res.send(
+    '<h1>Jobs API - Server is running successfully</h1><a href="/api-docs">Documentation</a>'
+  );
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
